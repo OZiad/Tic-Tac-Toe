d49640d2e3aada8ebe3e0a432665d184d5c09b6b
Omar Ziad <omar.ziad1@outlook.com>
2023-05-12 18:04:38 -0400

    Add test for token placement

--- src/Google_tests/TicTacToeTests.cpp
+++ src/Google_tests/TicTacToeTests.cpp
@@ -4,16 +4,21 @@
 class TicTacToeTest : public ::testing::Test
 {
  protected:
-	TicTacToe game;
+	TicTacToe game = TicTacToe{ "player1", "player2" };
 	void SetUp() override
 	{
-		game = TicTacToe({ "player1", "player2" });
 	}
 };
 TEST_F(TicTacToeTest, CheckPlayerNames)
 {
 	EXPECT_EQ(game.getPlayer1(), "player1");
 	EXPECT_EQ(game.getPlayer2(), "player2");
+}
+TEST_F(TicTacToeTest, VerifyTokenIsPlaced)
+{
+	game.placeToken(4);
+	std::array<std::array<char, 3>, 3> board = game.getBoard();
+	ASSERT_EQ(board[1][0], 'X');
 }
 
 
